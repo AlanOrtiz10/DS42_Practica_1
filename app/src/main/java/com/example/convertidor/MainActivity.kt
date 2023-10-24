9d097f6e804c386dec71893672a436241f9d6477
Alan <a22311050@uthermosillo.edu.mx>
2023-10-23 22:18:59 -0700

    Se crearon las formulas de conversión

--- app/src/main/java/com/example/convertidor/MainActivity.kt
+++ app/src/main/java/com/example/convertidor/MainActivity.kt
@@ -13,34 +13,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var selectedTemp = findViewById<Spinner>(R.id.temp_options)
-        var temp = findViewById<EditText>(R.id.cantidad)
-        var button = findViewById<Button>(R.id.convertir)
-        var result = findViewById<TextView>(R.id.resultado)
+        val selectedTemp = findViewById<Spinner>(R.id.temp_options)
+        val temp = findViewById<EditText>(R.id.cantidad)
+        val button = findViewById<Button>(R.id.convertir)
+        val result = findViewById<TextView>(R.id.resultado)
 
-
-        if(selectedTemp != null){
-            val adapter = ArrayAdapter (
+        if (selectedTemp != null) {
+            val adapter = ArrayAdapter(
                 this,
                 android.R.layout.simple_spinner_item,
                 resources.getStringArray(R.array.convert_options)
             )
-
             selectedTemp.adapter = adapter
         }
 
         button.setOnClickListener {
+            convert(temp.text.toString(), result)
+        }
+    }
 
-            temp.text
+    fun convert(celsiusString: String, result: TextView) {
+        // °C to °F Formula
+        if (celsiusString.isNotEmpty()) {
+            val celsius = celsiusString.toDouble()
+            val fahrenheit = (celsius * 9 / 5) + 32
+            result.text = "Resultado: $fahrenheit °F"
+        } else {
+            result.text = "Ingresa un valor válido en grados Celsius."
         }
 
-        fun convert(){
-            //TODO Crear las conversiones
-            //Tarea: Traer las formulas de conversión
+     fun convertCK(CelsiusK: String, result: TextView){
+         // °C to °K
+         if(CelsiusK.isNotEmpty()){
+             val celsiusK = CelsiusK.toDouble()
+             val kelvin = (celsiusK + 273.15)
+             result.text = "Resultado: $kelvin °K"
+         } else {
+            result.text = "Ingresa un valor válido en grados Kelvin."
         }
+     }
+        //°F a °C: Celsius (°C) = (Fahrenheit (°F) - 32) × 5/9
+        //°F a °K: Kelvin (K) = (Fahrenheit (°F) - 32) × 5/9 + 273.15
+        //°K a °C: Celsius (°C) = Kelvin (K) - 273.15
+        //°K a °F: Fahrenheit (°F) = (Kelvin (K) - 273.15) × 9/5 + 32
+
 
 
     }
-
-
 }
